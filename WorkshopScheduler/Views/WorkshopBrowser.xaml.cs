32ddf1c47d1bea201d93f9e47920da92ba1ce096
Kuba <kuba.krymarys@gmail.com>
2018-02-28 14:16:01 +0100

    * App.xaml.cs: * WorkshopScheduler.csproj: * WorkshopBrowser.xaml.cs:
    * WorkshopBrowser.xaml: created ListView with custom template for
      wrkshp
    
    * Workshop.cs: added basic fields for workshop

---
+++ WorkshopScheduler/Views/WorkshopBrowser.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace WorkshopScheduler
+{
+    public partial class WorkshopBrowser : ContentPage
+    {
+
+        var workshops_list = new List<workshop>
+
+
+        public WorkshopBrowser()
+        {
+            InitializeComponent();
+        }
+    }
+}
