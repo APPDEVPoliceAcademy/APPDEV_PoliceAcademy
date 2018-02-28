ce164fada79546529210c3d6502c70480e89c495
Kuba <kuba.krymarys@gmail.com>
2018-02-28 15:29:28 +0100

    * Resource.designer.cs: Few things added. it should populate list with one entry, and it does, however there is   problem with binding. So far no idea why
    * Workshop.cs:
    * WorkshopScheduler.csproj:
    * AssemblyInfo.cs:
    * WorkshopBrowser.xaml:
    * WorkshopBrowser.xaml.cs:

--- WorkshopScheduler/Views/WorkshopBrowser.xaml.cs
+++ WorkshopScheduler/Views/WorkshopBrowser.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace WorkshopScheduler
@@ -8,12 +8,21 @@ namespace WorkshopScheduler
     public partial class WorkshopBrowser : ContentPage
     {
 
-        var workshops_list = new List<workshop>
-
-
         public WorkshopBrowser()
         {
             InitializeComponent();
+            //lorem ipsum is to test longer strings, but I dont want to deal with them normally ;) 
+            String loremipsum = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.";
+
+            var workshops_list = new List<Workshop>();
+            workshops_list.Add(new Workshop() {
+               Title =  "Project Management",
+                Description =  loremipsum,
+                Date = new DateTime(2018,06,17),
+                 Coach = "Andrzej Nowak",
+                Place = "Windesheim"});
+
+            workshopsList.ItemsSource = workshops_list; 
         }
     }
 }
