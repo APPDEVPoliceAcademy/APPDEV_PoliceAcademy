8410eb9a7320a29463e7ff43f78b0674f92aeee8
jkrymarys <kuba.krymarys@gmail.com>
2018-03-03 11:21:14 +0100

    search all workshops by title functionality added

--- WorkshopScheduler/Views/WorkshopBrowser.xaml.cs
+++ WorkshopScheduler/Views/WorkshopBrowser.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Xamarin.Forms;
 
 namespace WorkshopScheduler
@@ -8,28 +9,70 @@ namespace WorkshopScheduler
     public partial class WorkshopBrowser : ContentPage
     {
 
+        List<Workshop> workshopsList;
+
         public WorkshopBrowser()
         {
             InitializeComponent();
             //lorem ipsum is to test longer strings, but I dont want to deal with them normally ;) 
             String loremipsum = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.";
 
-            var workshops_list = new List<Workshop>();
+            workshopsList = new List<Workshop>();
 
-            workshops_list.Add(new Workshop() {
+            workshopsList.Add(new Workshop() {
                Title =  "Project Management",
                 ShortDescription = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit",
                 Description =  loremipsum,
                 Date = new DateTime(2018,06,17),
-                 Coach = "Andrzej Nowak",
+                Coach = "Andrzej Nowak",
                 Place = "Windesheim"});
 
-            workshopsList.ItemsSource = workshops_list; 
+            workshopsList.Add(new Workshop()
+            {
+                Title = "Test 2",
+                ShortDescription = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit",
+                Description = loremipsum,
+                Date = new DateTime(2018, 06, 19),
+                Coach = "Tadeusz Sznuk",
+                Place = "on-line"
+            });
+
+            workshopsList.Add(new Workshop()
+            {
+                Title = "Halina,Zygfryd Harry i dziewczyna bez szpary",
+                ShortDescription = "Wyszukajna youtube.... ale na własne ryzyko Ptysiu ;* \n @Melvin sorry, in Enlish this not funny :<" +
+                    "" +
+                    "",
+                Description = loremipsum,
+                Date = new DateTime(2018, 06, 21), 
+                Coach = "Andrzej Norek",
+                Place = "Łódź"
+            });
+
+            workshopsListView.ItemsSource = workshopsList; 
         }
 
         void onSignIn (object sender, System.EventArgs e)
         {
             DisplayAlert("Sign in", "Are you sure?","Yes","No");
+        }
+
+        void searchboxChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+                List<Workshop> displayList;
+                toTest.Text = searchWorkshop.Text;
+
+            if(searchWorkshop.Text != null)
+            {
+                displayList = workshopsList.Where(x => x.Title.Contains(searchWorkshop.Text)).ToList();
+                workshopsListView.ItemsSource = displayList; 
+            }else
+            {
+                workshopsListView.ItemsSource = workshopsList;
+            }
+           
+           
+
         }
     }
 }
