9a01be64f72f0e323093f5cd5691ff80dd7536ce
Maciej <wyrzuc.maciej@gmail.com>
2018-03-20 10:12:46 +0100

    Added Profile and Settings Page

--- WorkshopScheduler/App.xaml.cs
+++ WorkshopScheduler/App.xaml.cs
@@ -1,14 +1,19 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 using WorkshopScheduler.Views;
 
 namespace WorkshopScheduler
 {
     public partial class App : Application
     {
+        private const string UserNameKey = "UserName";
+        private const string UserSurnameKey = "UserSurname";
+        private const string UserUnitKey = "UserUnit";
+
         public App()
         {
             InitializeComponent();
-            MainPage = new ToolbarView();
+            MainPage = new MainView();
         }
 
         protected override void OnStart()
@@ -26,5 +31,24 @@ namespace WorkshopScheduler
         {
             // Handle when your app resumes
         }
+
+        public String UserName
+        {
+            get => Properties.ContainsKey(UserNameKey) ? Properties[UserNameKey].ToString() : "";
+            set => Properties[UserNameKey] = value;
+        }
+
+        public String UserSurname
+        {
+            get => Properties.ContainsKey(UserSurnameKey) ? Properties[UserSurnameKey].ToString() : "";
+            set => Properties[UserSurnameKey] = value;
+        }
+
+        public String UserUnit
+        {
+            get => Properties.ContainsKey(UserUnitKey) ? Properties[UserUnitKey].ToString() : "";
+            set => Properties[UserUnitKey] = value;
+        }
+
     }
 }
