cae43691612eef657035a8fdb700a5c077e7d468
Jakub <krymarys.jakub@gmail.com>
2018-03-28 10:17:51 +0200

    basic filtering and sorting done

--- WorkshopScheduler/Views/ReservedBrowser.cs
+++ WorkshopScheduler/Views/ReservedBrowser.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using Xamarin.Forms;
 using WorkshopScheduler.Models;
+using WorkshopScheduler.Logic;
 
 
 namespace WorkshopScheduler.Views
@@ -12,6 +13,9 @@ namespace WorkshopScheduler.Views
     public partial class ReservedBrowser : ContentPage
     {
         ObservableCollection<Workshop> reservedList;
+        ObservableCollection<Workshop> displayList;
+        FiltersModalView filtersView;
+
 
         public ReservedBrowser()
         {
@@ -19,23 +23,88 @@ namespace WorkshopScheduler.Views
             //lorem ipsum is to test longer strings, but I dont want to deal with them normally ;) 
 
             reservedList = TestData.LoremIpsumData; // provide the test
-
             WorkshopsListView.ItemsSource = reservedList;
+            Sorting sortings = new Sorting();
+            Filters filters = new Filters();
+
+            filtersView = new FiltersModalView();
+
+            filtersView.SortingChanged += (o, sortingChosen) =>
+            {
+                displayList = null;
+
+                switch (sortingChosen)
+                {
+                    case SortingsEnum.ByDateAscending:
+                        displayList = sortings.ByDateAscending(reservedList);
+                        break;
+                    case SortingsEnum.ByDateDescending:
+                        displayList = sortings.ByDateDescending(reservedList);
+                        break;
+                    case SortingsEnum.ByTitleAscending:
+                        displayList = sortings.ByTitleAscending(reservedList);
+                        break;
+                    case SortingsEnum.ByTitleDescending:
+                        displayList = sortings.ByTitleDescending(reservedList);
+                        break;
+                    case SortingsEnum.None:
+                        displayList = reservedList;
+                        break;
+                    default:
+                        DisplayAlert("couldn't match any", "shit", "ok");
+                        break;
+                };
+
+
+                WorkshopsListView.ItemsSource = displayList;
+
+            };
+
+            filtersView.DatesFilterChanged += (o, dates) =>
+            {
+                displayList = filters.FilterByDate(reservedList, dates);
+                WorkshopsListView.ItemsSource = displayList;
+            };
+
+            filtersView.WeeksFilterChanged += (o, flag) =>
+            {
+                displayList = filters.FilterBy12weeks(reservedList, flag);
+                WorkshopsListView.ItemsSource = displayList;
+            };
+
+            filtersView.PlaceFilterChanged += (o, place) =>
+            {
+                displayList = filters.FilterByPlace(reservedList, place.ToString());
+                WorkshopsListView.ItemsSource = displayList;
+            };
+
+            filtersView.ResetSettings += (o, s) => {
+                WorkshopsListView.ItemsSource = reservedList;
+            };
         }
 
 
-        private void SearchWorkshop_OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+         void SearchWorkshop_OnPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             if (SearchWorkshop.Text != null)
             {
-                var displayList = reservedList.Where(x =>
-                    x.Title.IndexOf(SearchWorkshop.Text.Trim(' '), 0, StringComparison.CurrentCultureIgnoreCase) != -1).ToList();
-                WorkshopsListView.ItemsSource = displayList;
+                WorkshopsListView.ItemsSource = displayList.Where(x =>
+                                                x.Title.IndexOf(SearchWorkshop.Text.Trim(' '), 0, StringComparison.CurrentCultureIgnoreCase) != -1).ToList();
+
             }
             else
             {
                 WorkshopsListView.ItemsSource = reservedList;
             }
         }
+
+        async void SortingsButton_OnClicked(object sender, System.EventArgs e)
+        {
+            // DisplayAlert("refreshed",sortingChosen.ToString(),"ok");
+
+            await Navigation.PushModalAsync(filtersView);
+
+        }
     }
+
 }
