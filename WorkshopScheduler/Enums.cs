c62e09c4933a91738a98fdfe0efe23da687b7199
Jakub Krymarys <krymarys.jakub@gmail.com>
2018-04-04 12:57:24 +0200

    improved profile

--- WorkshopScheduler/Enums.cs
+++ WorkshopScheduler/Enums.cs
@@ -3,7 +3,7 @@ namespace WorkshopScheduler
 {
     public enum SortingsEnum { None, ByDateAscending, ByDateDescending, ByTitleAscending, ByTitleDescending };
 
-    public enum PlacesEnum { Zwolle, Apeldoorn };
+    public enum PlacesEnum { Nord,Soud, Oost, Westen  };
 
     public enum CategoryEnum { Zwolle, Apeldoorn };
 
--- WorkshopScheduler/Views/ProfilePage.xaml.cs
+++ WorkshopScheduler/Views/ProfilePage.xaml.cs
@@ -7,11 +7,13 @@ using WorkshopScheduler.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+
 namespace WorkshopScheduler.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfilePage : ContentPage
     {
+        List<PlacesEnum> placesSortings = Enum.GetValues(typeof(PlacesEnum)).Cast<PlacesEnum>().ToList();
 
         private App _currentApp = Application.Current as App;
         private List<Unit> _pickerUnitsOptions = Enum.GetValues(typeof(Unit)).Cast<Unit>().ToList();
@@ -19,6 +21,7 @@ namespace WorkshopScheduler.Views
         public ProfilePage()
         {
             InitializeComponent();
+            UnitPicker.ItemsSource = placesSortings;
             NameCell.Text = _currentApp.UserName;
             SurnameCell.Text = _currentApp.UserSurname;
             UnitPicker.ItemsSource = _pickerUnitsOptions;
