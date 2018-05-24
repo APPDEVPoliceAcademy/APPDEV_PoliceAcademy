041ee1ea1574407bc161ba2a2b48fa642da7103a
Maciej <wyrzuc.maciej@gmail.com>
2018-05-24 13:41:32 +0200

    Added defensive null check in filters, needs notification will be updated without need to reload, daymodalview updated to match listview form workshopsbrowser

--- WorkshopScheduler/Logic/Filters.cs
+++ WorkshopScheduler/Logic/Filters.cs
@@ -14,20 +14,17 @@ namespace WorkshopScheduler.Logic
 
         public ObservableCollection<WorkshopDTO> FilterByDate(ObservableCollection<WorkshopDTO> input, DateTime[] dates)
         {
-            return new ObservableCollection<WorkshopDTO>(input.Where(a => (a.Date >= dates[0].Date && a.Date <= dates[1].Date)));
+            return input == null ? new ObservableCollection<WorkshopDTO>() : new ObservableCollection<WorkshopDTO>(input?.Where(a => (a.Date >= dates[0].Date && a.Date <= dates[1].Date)));
         }
 
         public ObservableCollection<WorkshopDTO> FilterByPlace(ObservableCollection<WorkshopDTO> input, String desiredPlace)
         {
-            return new ObservableCollection<WorkshopDTO>(input.Where(a => (a.Place == desiredPlace)));
+            return input == null ? null : new ObservableCollection<WorkshopDTO>(input.Where(a => (a.Place == desiredPlace)));
         }
 
         public ObservableCollection<WorkshopDTO> FilterBy12Weeks(ObservableCollection<WorkshopDTO> input)
         {
-           /// if (flag)
-                return new ObservableCollection<WorkshopDTO>(input.Where(a => (a.Date <= DateTime.Now.AddDays(84)) && a.Date >= DateTime.Now));
-            
-              //  return input;
+            return input == null ? null : new ObservableCollection<WorkshopDTO>(input.Where(a => (a.Date <= DateTime.Now.AddDays(84)) && a.Date >= DateTime.Now));
         }
 
 
